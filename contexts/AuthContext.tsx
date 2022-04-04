db9e69e50fd6d2c39d7bfd1ccdea444fe2fb10bb
JosielMatos <josielmatos77@protonmail.com>
2022-04-04 06:26:35 -0300

    Add user info context

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,9 +1,16 @@
-import { createContext, ReactNode } from "react";
+import { useRouter } from "next/router";
+import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
 
 interface AuthProviderProps {
   children: ReactNode;
 }
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type signInCredentials = {
   email: string;
@@ -12,13 +19,16 @@ type signInCredentials = {
 
 interface IAuthContextData {
   signIn(credentials: signInCredentials): Promise<void>;
+  user: User;
   isAuthenticated: boolean;
 }
 
 export const AuthContext = createContext({} as IAuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
   const isAuthenticated = false;
+  const router = useRouter();
 
   async function signIn({ email, password }: signInCredentials) {
     try {
@@ -26,14 +36,23 @@ export function AuthProvider({ children }: AuthProviderProps) {
         email,
         password,
       });
-      console.log(response.data);
+
+      const { permissions, roles } = response.data;
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      router.push("/dashboard");
     } catch (error) {
       console.log(error);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
