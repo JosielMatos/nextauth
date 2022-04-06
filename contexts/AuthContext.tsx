20aabd8676ba67247e12afa4ae65dab4b228032b
JosielMatos <josielmatos77@protonmail.com>
2022-04-06 06:09:52 -0300

    Add cookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { createContext, ReactNode, useState } from "react";
+import { setCookie } from "nookies";
 import { api } from "../services/api";
 
 interface AuthProviderProps {
@@ -37,7 +38,17 @@ export function AuthProvider({ children }: AuthProviderProps) {
         password,
       });
 
-      const { permissions, roles } = response.data;
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 dias
+        path: "/",
+      });
+
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 dias
+        path: "/",
+      });
 
       setUser({
         email,
